56faec38baeda884eb37c482082b19c31e65d334
felipefilp <felipe_m_souza@yahoo.com.br>
2025-01-27 20:31:47 -0300

    ajustes server-side

--- src/download/download.service.ts
+++ src/download/download.service.ts
@@ -8,6 +8,16 @@ export class DownloadService {
             if(!ytdl.validateURL(url)){
                 throw new Error("URL inválida!")
             }
-            return ytdl(url, { quality: 'highest'});
-        }
+            return ytdl(url, {
+                quality: 'highest',
+                requestOptions: {
+                    headers: {
+                      'User-Agent':
+                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                      'Accept-Language': 'en-US,en;q=0.9',
+                      'Accept-Encoding': 'gzip, deflate, br',
+                    },
+            }}
+        )
     }
+}
